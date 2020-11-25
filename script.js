2d24f2387e19544d33e948ff7854061ba17d8e20
Asmina Barkhandinova <bar.asmina@gmail.com>
2020-11-25 15:41:58 +0600

    script added

---
+++ script.js
@@ -0,0 +1,47 @@
+
+let bestsellers = [
+	{
+		"name": "Green Ecler",
+		"src": "photo1.png"
+	},
+
+	{
+		"name": "Malevich Cupcakes",
+		"src": "photo2.png"
+	},
+
+	{
+		"name": "Chocolate",
+		"src": "photo3.png"
+	},
+
+	{
+		"name": "Red Velvet",
+		"src": "photo4.png"
+	},
+
+	{
+		"name": "Lemon Cake",
+		"src": "photo5.png"
+	},
+
+	{
+		"name": "Homemade Biscuit",
+		"src": "photo6.png"
+	}
+
+];
+
+let gallery = document.querySelector("#grid-container");
+function addCakes() {
+
+	for(let cake of bestsellers){
+		let item = document.createElement('div');
+		item.classList.add('grid-item');
+		item.innerHTML += `<img src="${cake.src}"><p>${cake.name}</p>`;
+		gallery.appendChild(item);
+	}
+	
+}
+
+addCakes();
